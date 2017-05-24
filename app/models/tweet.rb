b55a37c4130122999d8f1380e4b2543067747b23
Simon Reboul <simon.reboul@ynov.com>
2017-05-24 18:52:57 +0200

    Push 1

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,33 @@
-class Tweet < ApplicationRecord
+require 'grackle'
+
+class Tweet < ActiveRecord::Base
+
+MY_APPLICATION_NAME = "TweetSpace_Ingesup"
+  
+  def self.get_latest
+  	puts("Hello"); / Est affiché /
+  	tweets = client.user_timeline("gem")
+    /tweets = client.statuses.user_timeline? :screen_name => MY_APPLICATION_NAME # hit the API/
+    puts("Hello3"); / N'est pas affiché /
+    tweets.each do |t|
+      created = DateTime.parse(t.created_at)
+      unless Tweet.exists?(["created=?", created])
+        Tweet.create({:content => t.text, :created => created })
+       end
+    end
+    
+  end
+  
+  private
+  def self.client
+    Grackle::Client.new(:auth=>{
+      :type=>:oauth,
+      :consumer_key=>'Gzm6I93ZW8sR2LMbCpod7HOnf',
+      :consumer_secret=>'nIOyQmzYq2wnwYMvaNHjhNutjqXnD2uTNFbvsph5KpsqxdHtJ7',
+      :token=>"440215471-ViSDu9rTcC9OzWKmgq5cJRryALIOaJdxh28j3N0g",
+      :token_secret=>"xOkniXd5Pvg0QJLXxmtWbIou5wmq2aOJRZUhWy7mVMIXA"
+    })
+	client.ssl = true
+  end
 end
+
--- db/migrate/20170516144731_create_tweets.rb
+++ db/migrate/20170516144731_create_tweets.rb
@@ -1,11 +1,23 @@
 class CreateTweets < ActiveRecord::Migration[5.0]
+   def self.up
+    create_table :tweets do |t|
+      t.string :content
+      t.datetime :created
+    end
+  end
+
+  def self.down
+    drop_table :tweets
+  end
+end
+
   def change
-    create_table : do |t|
-      t.Date :date
+    create_table :tweets do |t|
+      t.date :date
       t.string :url
-      t.string: url_image
-      t.string: auteur
-      t.integer: likes
+      t.string :url_image
+      t.string :auteur
+      t.integer :likes
       t.text :description
       t.timestamps
     end
